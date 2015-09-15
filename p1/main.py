17f9ae1629e89fa3ad1789d0a609a639d2ec4ff9
Stephen Brennan <smb196@case.edu>
2015-09-15 13:16:22 -0400

    P1: AutoPEP8.

--- p1/main.py
+++ p1/main.py
@@ -23,7 +23,7 @@ from feature_selection import PCA
 
 
 CLASSIFIERS = {
-    'dtree'                     : DecisionTree,
+    'dtree': DecisionTree,
     #'linear_svm'                : LinearSupportVectorMachine,
     #'nbayes'                    : NaiveBayes,
     #'logistic_regression'       : LogisticRegression,
@@ -65,7 +65,7 @@ def get_folds(X, y, k):
     @param k : number of folds
     @return (train_X, train_y, test_X, test_y) for each fold
     """
-    pass #add code here
+    pass  # add code here
 
     return zip(train_X, train_y, test_X, test_y)
 
@@ -77,19 +77,22 @@ def main(**options):
 
     if "meta_algorithm" in options and "meta_iters" not in options:
         """ Make sure they use --meta-iters if they want to do bagging/boosting """
-        raise ValueError("Please indicate number of iterations for %s" % options["meta_algorithm"])
+        raise ValueError("Please indicate number of iterations for %s" % options[
+                         "meta_algorithm"])
 
     fs_alg = None
     if "fs_algorithm" in options:
         fs_alg = options.pop("fs_algorithm")
         if "fs_features" not in options:
-            raise ValueError("Please indicate number of features for %s" % fs_alg)
+            raise ValueError(
+                "Please indicate number of features for %s" % fs_alg)
         fs_n = options.pop("fs_features")
 
     schema, X, y = get_dataset(dataset, dataset_directory)
     folds = get_folds(X, y, k)
     stats_manager = StatisticsManager()
-    import pdb;pdb.set_trace()
+    import pdb
+    pdb.set_trace()
     for train_X, train_y, test_X, test_y in folds:
 
         # Construct classifier instance
@@ -110,23 +113,21 @@ def main(**options):
         predictions = classifier.predict(test_X)
         scores = classifier.predict_proba(test_X)
         if len(np.shape(scores)) > 1 and np.shape(scores)[1] > 1:
-            scores = scores[:,1]    # Get the column for label 1
+            scores = scores[:, 1]    # Get the column for label 1
         stats_manager.add_fold(test_y, predictions, scores, train_time)
 
-    print ('      Accuracy: %.03f %.03f'
-        % stats_manager.get_statistic('accuracy', pooled=False))
-		'''
-    print ('     Precision: %.03f %.03f'
-        % stats_manager.get_statistic('precision', pooled=False))
+    print('      Accuracy: %.03f %.03f'
+          % stats_manager.get_statistic('accuracy', pooled=False))
+        '''
+print ('     Precision: %.03f %.03f'
+% stats_manager.get_statistic('precision', pooled=False))
 
-    print ('        Recall: %.03f %.03f'
-        % stats_manager.get_statistic('recall', pooled=False))
+print ('        Recall: %.03f %.03f'
+% stats_manager.get_statistic('recall', pooled=False))
 
-    print ('Area under ROC: %.03f'
-        % stats_manager.get_statistic('auc', pooled=True))
-    '''   
-
-                
+print ('Area under ROC: %.03f'
+% stats_manager.get_statistic('auc', pooled=True))
+'''
 
 
 if __name__ == "__main__":
@@ -136,34 +137,49 @@ if __name__ == "__main__":
         description='Runs experiments for EECS 440.',
         argument_default=argparse.SUPPRESS)
 
-    parser.add_argument('--dataset_directory', dest='dataset_directory', metavar='DIR', default='.')
+    parser.add_argument('--dataset_directory',
+                        dest='dataset_directory', metavar='DIR', default='.')
     parser.add_argument('--k', metavar='FOLDS', type=int, default=5)
     parser.add_argument('--dataset', metavar='DATASET', default='voting')
-    parser.add_argument('--meta_algorithm', metavar='ALG', required=False, choices=['bagging', 'boosting'], help='Bagging or boosting, if desired')
-    parser.add_argument('--meta_iters', metavar='N', type=int, required=False, help='Iterations for bagging or boosting, if applicable')
-    parser.add_argument('--fs_algorithm', metavar='ALG', required=False, choices=['pca'], help='Feature selection algorithm, if desired')
-    parser.add_argument('--fs_features', metavar='N', required=False, type=int, help='Number of feature to select, if applicable')
+    parser.add_argument('--meta_algorithm', metavar='ALG', required=False,
+                        choices=['bagging', 'boosting'], help='Bagging or boosting, if desired')
+    parser.add_argument('--meta_iters', metavar='N', type=int, required=False,
+                        help='Iterations for bagging or boosting, if applicable')
+    parser.add_argument('--fs_algorithm', metavar='ALG', required=False,
+                        choices=['pca'], help='Feature selection algorithm, if desired')
+    parser.add_argument('--fs_features', metavar='N', required=False,
+                        type=int, help='Number of feature to select, if applicable')
 
-    subparsers = parser.add_subparsers(dest='classifier', help='Classifier options')
-    
+    subparsers = parser.add_subparsers(
+        dest='classifier', help='Classifier options')
+
     dtree_parser = subparsers.add_parser('dtree', help='Decision Tree')
-    dtree_parser.add_argument('--depth', type=int, help='Maximum depth for decision tree, 0 for None', default=2)
+    dtree_parser.add_argument(
+        '--depth', type=int, help='Maximum depth for decision tree, 0 for None', default=2)
 
     ann_parser = subparsers.add_parser('ann', help='Artificial Neural Network')
-    ann_parser.add_argument('--gamma', type=float, help='Weight decay coefficient', default=0.01)
-    ann_parser.add_argument('--layer_sizes', type=int, help='Number of hidden layers', default=3)
-    ann_parser.add_argument('--num_hidden', type=int, help='Number of hidden units in the hidden layers', default=40)
+    ann_parser.add_argument('--gamma', type=float,
+                            help='Weight decay coefficient', default=0.01)
+    ann_parser.add_argument('--layer_sizes', type=int,
+                            help='Number of hidden layers', default=3)
+    ann_parser.add_argument('--num_hidden', type=int,
+                            help='Number of hidden units in the hidden layers', default=40)
     ann_parser.add_argument('--epsilon', type=float, required=False)
     ann_parser.add_argument('--max_iters', type=int, required=False)
 
-    svm_parser = subparsers.add_parser('linear_svm', help='Linear Support Vector Machine')
-    svm_parser.add_argument('--c', type=float, help='Regularization parameter for SVM', default=1)
+    svm_parser = subparsers.add_parser(
+        'linear_svm', help='Linear Support Vector Machine')
+    svm_parser.add_argument(
+        '--c', type=float, help='Regularization parameter for SVM', default=1)
 
     nbayes_parser = subparsers.add_parser('nbayes', help='Naive Bayes')
-    nbayes_parser.add_argument('--alpha', type=float, help='Smoothing parameter for Naive Bayes', default=1)
+    nbayes_parser.add_argument(
+        '--alpha', type=float, help='Smoothing parameter for Naive Bayes', default=1)
 
-    lr_parser = subparsers.add_parser('logistic_regression', help='Logistic Regression')
-    lr_parser.add_argument('--c', type=float, help='Regularization parameter for Logistic Regression', default=10)
+    lr_parser = subparsers.add_parser(
+        'logistic_regression', help='Logistic Regression')
+    lr_parser.add_argument(
+        '--c', type=float, help='Regularization parameter for Logistic Regression', default=10)
 
     args = parser.parse_args()
     main(**vars(args))
