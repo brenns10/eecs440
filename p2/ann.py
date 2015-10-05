bf3305387e8b1f2a95ce634d0cfefe9f8ac3a1fd
Stephen Brennan <smb196@case.edu>
2015-10-05 13:57:07 -0400

    P1: Progress commit on fitting.

--- p2/ann.py
+++ p2/ann.py
@@ -6,6 +6,10 @@ import numpy as np
 import scipy
 
 
+def sigmoid(X):
+    return (1 + np.exp(-X)) ** -1
+
+
 class ArtificialNeuralNetwork(object):
 
     def __init__(self, gamma, layer_sizes, num_hidden, epsilon=None,
@@ -37,7 +41,7 @@ class ArtificialNeuralNetwork(object):
         self._num_attrs = len(schema.feature_names)
 
         # Create layer weights:
-        self._shape = (self._num_layers, self._num_hidden, self._num_attrs)
+        self._shape = (self._num_hidden, self._num_attrs)
         self._weights = np.random.uniform(-0.1, 0.1, shape=self._shape)
         self._out_weights = np.random.uniform(-0.1, 0.1,
                                               shape=self._num_hidden)
@@ -46,7 +50,20 @@ class ArtificialNeuralNetwork(object):
         """
         Fit a neural network of layer_sizes * num_hidden hidden units using X,y
         """
-        pass
+        # Create a 0-1 copy of y.
+        y01 = y.copy()
+        y01[y01 == -1] = 0
+
+        # Feed examples through network.
+        hidden_ns = np.dot(self._weights, X.T)
+        hidden_outs = sigmoid(hidden_ns)
+        outer_ns = np.dot(hidden_outs.T, self._out_weights)
+        outer_out = sigmoid(outer_ns)
+
+        # Get partial derivatives for outer unit.
+        outer_deriv = s * (1 - s) * (s - y01) * hidden_outs.T
+        # Add weight decay term
+        
 
     def predict(self, X):
         """ Predict -1/1 output """
