e0c7e05d96f01e8f891213fce79259c7d6d4a35b
Stephen Brennan <smb196@case.edu>
2015-11-16 22:35:23 -0500

    P4: quick and dirty bagger

--- p4/bagger.py
+++ p4/bagger.py
@@ -1,21 +1,21 @@
 """
 An implementation of bagging as a wrapper for a classifier
 """
+
 import numpy as np
-import scipy
 
 from dtree import DecisionTree
 from ann import ArtificialNeuralNetwork
-
-from nbayes import NaiveBayes
-from logistic_regression import LogisticRegression
+# from nbayes import NaiveBayes
+# from logistic_regression import LogisticRegression
 
 CLASSIFIERS = {
-    'dtree'                 : DecisionTree,
-    'ann'                   : ArtificialNeuralNetwork,
-    'nbayes'                : NaiveBayes,
-    'logistic_regression'   : LogisticRegression,
+    'dtree': DecisionTree,
+    'ann': ArtificialNeuralNetwork,
+    # 'nbayes'                : NaiveBayes,
+    # 'logistic_regression'   : LogisticRegression,
 }
+
 
 class Bagger(object):
 
@@ -29,13 +29,26 @@ class Bagger(object):
         @param iters : How many iterations of bagging to do
         @param params : Parameters for the classification algorithm
         """
-        pass
+        self._algorithm = algorithm
+        self._cls = CLASSIFIERS[self._algorithm]
+        self._iters = iters
+        self._params = params
+        self._classifiers = []
 
     def fit(self, X, y):
-        pass
+        for i in range(self._iters):
+            print('Bagger: fit classifier %03d/%03d' % (i+1, self._iters))
+            indexer = np.random.randint(0, len(y), len(y))
+            new_X = X[indexer]
+            new_y = y[indexer]
+            classifier = self._cls(**self._params)
+            classifier.fit(new_X, new_y)
+            self._classifiers.append(classifier)
 
     def predict(self, X):
-        pass
+        return self.predict_proba(X) > 0.5
 
     def predict_proba(self, X):
-        pass
+        predictions = [c.predict(X) for c in self._classifiers]
+        positives = [(p == 1) for p in predictions]
+        return positives / self._iters
